fb48a5dfc31a14e66912a58bcef148a348ebd754
sevgiakten <sevgiakten@gmail.com>
2023-02-16 16:10:06 +0300

    Deleted unnecessery files

--- OTHERS/LEAP/simple.py
+++
@@ -1,9 +0,0 @@
-from leap_ec.simple import ea_solve
-
-
-def f(x):
-    """A real-valued function to optimized."""
-    return sum(x) ** 2
-
-
-ea_solve(f, bounds=[(-5.12, 5.12) for _ in range(5)], maximize=True)
