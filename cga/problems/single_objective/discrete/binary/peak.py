2e99c9fd1d9da25a2b2c045df299917bbf269f37
sevgiakten <sevgiakten@gmail.com>
2023-03-17 17:14:24 +0300

    (#9) implement p-peak problem

---
+++ cga/problems/single_objective/discrete/binary/peak.py
@@ -0,0 +1,33 @@
+from problems.abstract_problem import AbstractProblem
+from numpy import random
+
+# P-PEAK Problem
+"""
+References
+    1. De Jong, K.A., Potter, M.A. & Spears, W.M. (1997). Using problem generators to explore the e®ects of epistasis. In T. Back, editor, Proceedings of the Seventh ICGA, Morgan Kaufmann, 338-345.
+    2. Alba, E., Dorronsoro, B., Giacobini, M., & Tomassini, M. (2004). Decentralized cellular evolutionary algorithms. International Journal of Applied Mathematics and Computer Science, 14(3), 101-117.
+"""
+# Length of chromosomes = 100
+# Maximum Fitness Value = 1.0
+
+
+class Peak(AbstractProblem):
+    def f(self, x: list) -> float:
+
+        random.seed(100)
+
+        problem_length = len(x)
+        number_of_peaks = 100
+        p_target = list()
+        p_target = [[random.randint(2) for g in range(100)]
+                    for h in range(100)]
+
+        for m in range(number_of_peaks):
+            distance = 0
+            for n in range(problem_length):
+                if (p_target[m][n] != x[m][n]):
+                    distance += 1
+
+        fitness = distance/100.0
+
+        return fitness
---
+++ cga/tests/test_peak.py
@@ -0,0 +1,26 @@
+from problems.single_objective.discrete.binary.peak import Peak
+from numpy import random
+
+""" Note:
+The max fitness of p-peak problem is equal to 1.0 and this is only possible when x (that represent chromosome) or p_target values be fielded opposite of 0 and 1 like this:
+    x = [[1 for g in range(100)]for h in range(100)]
+    p_target = [[0 for g in range(100)]for h in range(100)]
+When I did this test manually, I saw that it gave expected max fitness value, 1.0.
+Because p_target is fielded in Peak class, we cannot intervene it from test script. Maybe it is possibble to send p_target value as f function parameter, but I didn't need it at this stage. 
+"""
+
+
+def test_peak():
+
+    theproblem = Peak()
+
+    random.seed(45)
+    assert theproblem.f([[random.randint(2) for g in range(100)]
+                        for h in range(100)]) == 0.49
+
+    random.seed(50)
+    assert theproblem.f([[random.randint(2) for g in range(100)]
+                         for h in range(100)]) == 0.6
+    random.seed(100)
+    assert theproblem.f([[random.randint(2) for g in range(100)]
+                         for h in range(100)]) == 0.0
