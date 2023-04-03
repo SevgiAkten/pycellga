f05c9c8b8b8ce5fc714d70d1e20dcac9c62f7ab8
SevgiAkten <sevgiakten@gmail.com>
2023-04-03 15:17:14 +0300

    (#4) implement two point crossover with test

---
+++ cga/recombination/two_point_crossover.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+from individual import *
+from problems.abstract_problem import AbstractProblem
+from typing import List
+
+
+class TwoPointCrossover:
+    def __init__(self, parents: list, problem: AbstractProblem):
+        self.parents = parents
+        self.problem = problem
+
+    def get_recombinations(self) -> List[Individual]:
+
+        offsprings = []
+        p1 = self.parents[0]
+        p2 = self.parents[1]
+
+        parameter = len(p1.chromosome) * 0.20
+        min_co = parameter
+        max_co = len(p1.chromosome) - (parameter - 1)
+
+        co_point_1 = np.random.randint(min_co, max_co)
+        co_point_2 = np.random.randint(min_co, max_co)
+
+        P1_seg_1 = p1.chromosome[0:co_point_1]
+        P1_seg_2 = p1.chromosome[co_point_1: len(p1.chromosome)]
+
+        P2_seg_1 = p2.chromosome[0:co_point_2]
+        P2_seg_2 = p2.chromosome[co_point_2: len(p2.chromosome)]
+
+        seg_1_aux = list(p2.chromosome)
+        seg_2_aux = list(p1.chromosome)
+
+        # Birinci çocuk
+        rand_number = np.random.rand()
+
+        if rand_number < 0.5:
+
+            for i in range(len(P1_seg_1)):
+                seg_1_aux.remove(P1_seg_1[i])
+
+            new_chromosome_1 = P1_seg_1 + seg_1_aux
+        else:
+            for i in range(len(P1_seg_2)):
+                seg_1_aux.remove(P1_seg_2[i])
+
+            new_chromosome_1 = seg_1_aux + P1_seg_2
+
+        # First child
+        child_1 = Individual()
+        child_1.chromosome = new_chromosome_1
+        child_1.ch_size = len(new_chromosome_1)
+
+        child_1.position = p1.position
+        child_1.neighbors_positions = p1.neighbors_positions
+        child_1.fitness_value = self.problem.f(child_1.chromosome)
+        offsprings.append(child_1)
+
+        # İkinci çocuk
+        rand_number = np.random.rand()
+
+        if rand_number < 0.5:
+
+            for i in range(len(P2_seg_1)):
+                seg_2_aux.remove(P2_seg_1[i])
+
+            new_chromosome_2 = P2_seg_1 + seg_2_aux
+
+        else:
+            for i in range(len(P2_seg_2)):
+                seg_2_aux.remove(P2_seg_2[i])
+
+            new_chromosome_2 = seg_2_aux + P2_seg_2
+
+        # Second child
+        child_2 = Individual()
+        child_2.chromosome = new_chromosome_2
+        child_2.ch_size = len(new_chromosome_2)
+
+        child_2.position = p2.position
+        child_2.neighbors_positions = p2.neighbors_positions
+        child_2.fitness_value = self.problem.f(child_2.chromosome)
+        offsprings.append(child_2)
+
+        return offsprings
---
+++ cga/tests/test_two_point_crossover.py
@@ -0,0 +1,30 @@
+from problems.single_objective.discrete.binary.one_max import OneMax
+from recombination.two_point_crossover import TwoPointCrossover
+from individual import Individual
+
+
+def test_two_point_crossover():
+
+    CHSIZE = 10
+
+    indv1 = Individual(gen_type="Binary", ch_size=CHSIZE)
+    indv2 = Individual(gen_type="Binary", ch_size=CHSIZE)
+
+    indv1.randomize()
+    indv2.randomize()
+
+    parents = [indv1, indv2]
+
+    theproblem = OneMax()
+
+    ucx = TwoPointCrossover(parents, theproblem)
+    child1, child2 = ucx.get_recombinations()
+
+    assert child1.ch_size == child2.ch_size
+    assert child1.ch_size == CHSIZE
+
+    for i in range(CHSIZE):
+        assert child1.chromosome[i] == 1 or child1.chromosome[i] == 0
+
+    for i in range(CHSIZE):
+        assert child2.chromosome[i] == 1 or child2.chromosome[i] == 0
