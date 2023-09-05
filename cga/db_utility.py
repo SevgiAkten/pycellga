5e25265b7926f9884c978d99e3b69f76f69cffda
SevgiAkten <sevgiakten@gmail.com>
2023-09-05 16:38:00 +0300

    edit db_utility

--- cga/db_utility.py
+++ cga/db_utility.py
@@ -18,21 +18,45 @@ class DBUtility:
         self._conn_.commit()
 
     def createtables(self) -> None:
-        sql = """
-            CREATE TABLE IF NOT EXISTS Simulations 
-            (method text, testf text, p int, optimum float)
+        # sql = """
+        #     CREATE TABLE IF NOT EXISTS Simulations
+        #     (method text, testf text, p int, optimum float)
+        # """
+        sql2 = """
+            CREATE TABLE IF NOT EXISTS ExperimentResults
+            (
+            Method TEXT,
+            Gen_Type TEXT,
+            TestFunction TEXT,
+            Best_Solution FLOAT,
+            Found_at_Generation NUMERIC,
+            Selection TEXT,
+            Recombination TEXT,
+            Mutation TEXT,
+            Neighborhood TEXT,
+            Number_of_Columns INTEGER,
+            Number_of_Rows INTEGER,
+            Number_of_Generation INTEGER,
+            Probability_of_Crossover NUMERIC,
+            Probability_of_Mutation NUMERIC
+            )
         """
-        self.getcursor().execute(sql)
+        self.getcursor().execute(sql2)
         self.commit()
 
-    def insertoptresult(self, method: str, f: str, p: int, optimum: float):
-        sql = """
-            INSERT INTO Simulations 
-            (method, testf, p, optimum)
-            VALUES 
-            ('{}', '{}', {}, {})
-        """.format(method, f, p, optimum)
-        self.getcursor().execute(sql)
+    def insertoptresult(self, method: str, gen_type: str, test_function: str, best_solution: float, found_at_generation: int, selection: str, recombination: str, mutation: str, neighborhood: str, n_cols: int, n_rows: int, n_gen: int, p_cross: float, p_mut: float):
+        # sql = """
+        #     INSERT INTO Simulations
+        #     (method, testf, p, optimum)
+        #     VALUES
+        #     ('{}', '{}', {}, {})
+        # """.format(method, f, p, optimum)
+
+        sql2 = """
+            INSERT INTO ExperimentResults VALUES ('{}','{}','{}',{},{},'{}','{}','{}','{}','{}',{},{},{},{})
+        """.format(method, gen_type, test_function, best_solution, found_at_generation, selection, recombination, mutation, neighborhood, n_cols, n_rows, n_gen, p_cross, p_mut)
+
+        self.getcursor().execute(sql2)
         self.commit()
 
     def closedb(self) -> None:
--- cga/main_cga_2.py
+++ cga/main_cga_2.py
@@ -1,19 +1,31 @@
-# from optimizer import *
-from optimizer_sync import *
+
+# --------------------------------- methods ------------------------------------------ #
+# from optimizer_cga import *
+# from optimizer_sync_cga import *
+from optimizer_alpha_cga import *
+
+# ------------------------------------------------------------------------------------ #
+
 import matplotlib.pyplot as plt
+from db_utility import DBUtility
 
 # --------------------------------- problems -------------------------------- #
 # Binary
 from problems.single_objective.discrete.binary.count_sat import CountSat
-from problems.single_objective.discrete.binary.fms import Fms
+# from problems.single_objective.discrete.binary.fms import Fms
 from problems.single_objective.discrete.binary.mmdp import Mmdp
 from problems.single_objective.discrete.binary.one_max import OneMax
 from problems.single_objective.discrete.binary.peak import Peak
+from problems.single_objective.discrete.binary.ecc import Ecc
+from problems.single_objective.discrete.binary.maxcut20_01 import Maxcut20_01
+from problems.single_objective.discrete.binary.maxcut20_09 import Maxcut20_09
+from problems.single_objective.discrete.binary.maxcut100 import Maxcut100
 # Permutation
 from problems.single_objective.discrete.permutation.tsp import Tsp
 # Real-valued
 from problems.single_objective.continuous.ackley import Ackley
 from problems.single_objective.continuous.bohachevsky import Bohachevsky
+from problems.single_objective.continuous.fms import Fms
 from problems.single_objective.continuous.rastrigin import Rastrigin
 from problems.single_objective.continuous.rosenbrock import Rosenbrock
 from problems.single_objective.continuous.schwefel import Schwefel
@@ -41,37 +53,65 @@ from mutation.two_opt_mutation import TwoOptMutation
 # -------------------------------------------------------------------------- #
 
 
-result_tuple = optimize(
-    n_cols=5,
-    n_rows=5,
-    n_gen=100,
-    ch_size=14,
-    gen_type="Permutation",
-    p_crossover=0.9,
-    p_mutation=0.6,
-    known_best=30.878500,
-    k_tournament=2,
-    problem=Tsp(),
-    selection=TournamentSelection,
-    recombination=PMXCrossover,
-    mutation=SwapMutation
-)
-# result_tuple[0] = optimizer_result, type is dict
-# result_tuple[1] = parameters, type is dict
-# result_tuple[2] = best_objectives, type is list
-# result_tuple[3] = avg_objectives, type is list
+def runSimulation():
+    result_tuple = optimize(
+        n_cols=5,
+        n_rows=5,
+        n_gen=500,
+        ch_size=14,
+        gen_type="Permutation",
+        p_crossover=0.7,
+        p_mutation=0.5,
+        known_best=3323,
+        k_tournament=2,
+        problem=Tsp(),
+        selection=TournamentSelection,
+        recombination=PMXCrossover,
+        mutation=SwapMutation
+    )
+    # result_tuple[0] = optimizer_result, type is dict
+    # result_tuple[1] = parameters, type is dict
+    # result_tuple[2] = best_objectives, type is list
+    # result_tuple[3] = avg_objectives, type is list
 
-# exucute optimize function print optimizer_result dict keys and values
-print("-------------#### Solution Output ####-------------")
-[print(i, result_tuple[0][i]) for i in result_tuple[0]]
+    method = "cga"
+    gen_type = "Permutation"
+    test_function = "Tsp"
+    best_solution = result_tuple[0].get('best_solution')
+    found_at_generation = result_tuple[0].get('found_at_generation')
+    selection = "TournamentSelection"
+    recombination = "PMXCrossover"
+    mutation = "SwapMutation"
+    neighborhood = "Linear9"
+    n_cols = 5
+    n_rows = 5
+    n_gen = result_tuple[1].get('number_of_generation')
+    p_cross = result_tuple[1].get('probability_of_crossover')
+    p_mut = result_tuple[1].get('probability_of_mutation')
 
-print("-------------##### Parameters #####-------------")
-[print(i, result_tuple[1][i]) for i in result_tuple[1]]
+    # database record
+    tmpdb = "./simulations.db"
+    db = DBUtility(dbpath=tmpdb)
+    db.createtables()
+    db.insertoptresult(method, gen_type, test_function, best_solution, found_at_generation,
+                       selection, recombination, mutation, neighborhood, n_cols, n_rows, n_gen, p_cross, p_mut)
+    db.closedb()
 
-# plot result
-plt.plot(result_tuple[2])  # best_objectives
-plt.plot(result_tuple[3])  # avg_objectives
-plt.title("Objectives", fontsize=20, fontweight="bold"),
-plt.xlabel("Generations", fontsize=16, fontweight="bold"),
-plt.ylabel("Cost", fontsize=16, fontweight="bold"),
-plt.show()
+    # exucute optimize function print optimizer_result dict keys and values
+    print("-------------#### Solution Output ####-------------")
+    [print(i, result_tuple[0][i]) for i in result_tuple[0]]
+
+    print("-------------##### Parameters #####-------------")
+    [print(i, result_tuple[1][i]) for i in result_tuple[1]]
+
+    # plot result
+    plt.plot(result_tuple[2])  # best_objectives
+    plt.plot(result_tuple[3])  # avg_objectives
+    plt.title("Objectives", fontsize=20, fontweight="bold"),
+    plt.xlabel("Generations", fontsize=16, fontweight="bold"),
+    plt.ylabel("Cost", fontsize=16, fontweight="bold"),
+    # plt.show()
+
+
+for x in range(10):
+    runSimulation()
