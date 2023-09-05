b00e6ef94d491fc6fcd07b5e5b0429592015f729
SevgiAkten <sevgiakten@gmail.com>
2023-09-05 16:35:10 +0300

    edit tsp and add ulysses16 data

--- cga/problems/single_objective/discrete/permutation/tsp.py
+++ cga/problems/single_objective/discrete/permutation/tsp.py
@@ -2,17 +2,28 @@ from problems.abstract_problem import AbstractProblem
 import tsplib95
 from math import sqrt
 import pandas as pd
-#### berlin_52 #################################
+from geopy.distance import geodesic
+
+#### berlin52.tsp ########################################
+# EDGE_WEIGHT_TYPE: EUC_2D,   use euclidean_dist function
 # Length of chromosomes = 52
 # Known Best Route = [1, 49, 32, 45, 19, 41, 8, 9, 10, 43, 33, 51, 11, 52, 14, 13, 47, 26, 27, 28, 12, 25, 4, 6, 15, 5, 24, 48, 38, 37, 40, 39, 36, 35, 34, 44, 46, 16, 29, 50, 20, 23, 30, 2, 7, 42, 21, 17, 3, 18, 31, 22]
-# Minumum Fitness Value = 7544.365901904087
-################################################
+# Minumum Fitness Value = 7544.3659 some resources its 7442
+#########################################################
 
-#### burma_14 #################################
+#### burma14.tsp ########################################
+# EDGE_WEIGHT_TYPE: GEO,   use gographical_dist function
 # Length of chromosomes = 14
 # Known Best Route = []
-# Minumum Fitness Value = 30.878500
-################################################
+# Minumum Fitness Value = 3323
+#########################################################
+
+#### ulysses16.tsp ######################################
+# EDGE_WEIGHT_TYPE: GEO,   use gographical_dist function
+# Length of chromosomes = 16
+# Known Best Route = [1, 14, 13, 12, 7, 6, 15, 5, 11, 9, 10, 16, 3, 2, 4, 8]
+# Minumum Fitness Value = 6859
+##########################################################
 
 
 class Tsp(AbstractProblem):
@@ -33,7 +44,7 @@ class Tsp(AbstractProblem):
         for i in nodes:
             temp_row = []
             for j in nodes:
-                temp_row.append(self.euclidean_dist(i, j))
+                temp_row.append(self.gographical_dist(i, j))
             temp.append(temp_row)
 
         node_name = []
@@ -54,8 +65,16 @@ class Tsp(AbstractProblem):
                 end_node = x[i+1]
             fitness += Dist[start_node][end_node]
 
-        return fitness
+        return round(fitness, 1)
 
     def euclidean_dist(self, a: list, b: list) -> float:
         dist = sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
-        return dist
+        return round(dist, 1)
+
+    def gographical_dist(self, a: list, b: list) -> float:
+        dist = 0.0
+        for i in range(len(a)):
+            x_city = ([a[0], a[1]])
+            y_city = ([b[0], b[1]])
+            dist = geodesic(x_city, y_city).km
+        return round(dist, 1)
--- cga/tests/test_tsp.py
+++ cga/tests/test_tsp.py
@@ -9,15 +9,15 @@ def test_tsp():
 
     random.seed(0)
     assert theproblem.f(
-        list(random.sample(range(1, 53), 52))) == 29495.465603578683
+        list(random.sample(range(1, 53), 52))) == 29495.4656
     random.seed(50)
     assert theproblem.f(
-        list(random.sample(range(1, 53), 52))) == 30518.155190953657
+        list(random.sample(range(1, 53), 52))) == 30518.1551
     random.seed(100)
     assert theproblem.f(
-        list(random.sample(range(1, 53), 52))) == 31545.914565031067
+        list(random.sample(range(1, 53), 52))) == 31545.9145
 
     # Optimal tour
     with open("berlin52.opt.tour.txt") as o:
         best_route = tsplib95.read(o)
-    assert theproblem.f(best_route.tours[0]) == 7544.365901904087
+    assert theproblem.f(best_route.tours[0]) == 7544.3659
