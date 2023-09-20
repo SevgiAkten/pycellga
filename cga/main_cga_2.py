0bdae90b80af7d0aaa2584cda074b093cc9f481f
SevgiAkten <sevgiakten@gmail.com>
2023-09-20 12:47:54 +0300

    (#4) implement blxalpha_crossover

--- cga/main_cga_2.py
+++ cga/main_cga_2.py
@@ -48,6 +48,7 @@ from recombination.byte_one_point_crossover import ByteOnePointCrossover
 from recombination.byte_one_point_crossover import ByteOnePointCrossover
 from recombination.flat_crossover import FlatCrossover
 from recombination.arithmetic_crossover import ArithmeticCrossover
+from recombination.blxalpha_crossover import BlxalphaCrossover
 
 # -------------------------------------------------------------------------- #
 
@@ -75,7 +76,7 @@ def runSimulation():
         k_tournament=2,
         problem=Ackley(),
         selection=TournamentSelection,
-        recombination=ArithmeticCrossover,
+        recombination=BlxalphaCrossover,
         mutation=ByteMutationRandom
     )
     # result_tuple[0] = optimizer_result, type is dict
