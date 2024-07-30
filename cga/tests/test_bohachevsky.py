cf51208677f0401f1a3f84246856cec25204ef73
SevgiAkten <sevgiakten@gmail.com>
2024-07-30 17:46:38 +0300

    edit some test problems with tests

--- cga/tests/test_bohachevsky.py
+++ cga/tests/test_bohachevsky.py
@@ -3,6 +3,6 @@ from problems.single_objective.continuous.bohachevsky import Bohachevsky
 
 def test_bohachevsky():
     theproblem = Bohachevsky()
-    assert theproblem.f([2.305, -4.025, 3.805, -1.505]) == 103.5844151636286
-    assert theproblem.f([-4.995, -2.230, -3.706, 2.305]) == 95.58203266386136
-    assert theproblem.f([0 for i in range(10)]) == 0.0
+    assert theproblem.f([2.305, -4.025, 3.805, -1.505]) == round(103.5844151636286, 3)
+    assert theproblem.f([-4.995, -2.230, -3.706, 2.305]) == round(95.58203266386136,3)
+    assert theproblem.f([0 for i in range(10)]) == round(0.0, 3)
--- cga/tests/test_rastrigin.py
+++ cga/tests/test_rastrigin.py
@@ -3,6 +3,6 @@ from problems.single_objective.continuous.rastrigin import Rastrigin
 
 def test_rastrigin():
     theproblem = Rastrigin()
-    assert theproblem.f([2.305, -4.025, 3.805, -1.505]) == 78.37488219770594
-    assert theproblem.f([-4.995, -2.230, -3.706, 2.305]) == 83.83888661832582
-    assert theproblem.f([0 for i in range(10)]) == 0.0
+    assert theproblem.f([2.305, -4.025, 3.805, -1.505]) == round(78.37488219770594, 3)
+    assert theproblem.f([-4.995, -2.230, -3.706, 2.305]) == round(83.83888661832582,3)
+    assert theproblem.f([0 for i in range(10)]) == round(0.0, 3)
