ffe28846f0e92d3f8f87fd40eb38e9ebbc584afe
SevgiAkten <sevgiakten@gmail.com>
2024-08-06 11:01:18 +0300

    (#27) add other test functions an doc

--- cga/problems/single_objective/discrete/binary/ecc.py
+++ cga/problems/single_objective/discrete/binary/ecc.py
@@ -1,9 +1,11 @@
 from cga.problems.abstract_problem import AbstractProblem
+
 class Ecc(AbstractProblem):
     """
     Error Correcting Codes Design Problem (ECC) function implementation for optimization problems.
 
-    The ECC function is used for testing optimization algorithms, particularly those involving error-correcting codes.
+    The ECC function is used for testing optimization algorithms, particularly those involving 
+    error-correcting codes.
 
     Attributes
     ----------
@@ -34,24 +36,27 @@ class Ecc(AbstractProblem):
         float
             The ECC function value.
         """
-        individual_length = 12
-        half_code = 12
-        partial_fitness = 0.0
-        hamming = 0
-        fitness = 0.0
+        individual_length = 12  # Length of individual code segments
+        half_code = 12  # Number of code segments to compare
+        partial_fitness = 0.0  # Accumulated partial fitness value
+        fitness = 0.0  # Final fitness value
 
         for i in range(half_code):
             partial_fitness += 1.0 / (individual_length * individual_length)
             for j in range(half_code):
                 if j != i:
-                    hamming = 1
+                    hamming = 1  # Initialize Hamming distance
                     for k in range(individual_length):
+                        # Compute Hamming distance between segments
                         if x[(i * individual_length + k) ^ x[(j * individual_length + k)]]:
                             hamming += 1
 
+                    # Update partial fitness with contributions from current pair of segments
                     partial_fitness += (1.0 / (hamming * hamming) + 
-                                        1.0 / ((individual_length - hamming) * (individual_length - hamming)))
+                                        1.0 / ((individual_length - hamming) * 
+                                               (individual_length - hamming)))
 
+        # Calculate final fitness value
         fitness = 1.0 / (2 * partial_fitness)
 
         return round(fitness, 4)
---
+++ cga/tests/test_maxcut100.py
@@ -0,0 +1,36 @@
+import pytest
+
+from cga.problems.single_objective.discrete.binary.maxcut100 import Maxcut100
+
+
+@pytest.fixture
+def maxcut_instance():
+    """
+    Fixture for creating an instance of the Maxcut100 class.
+
+    This fixture returns an instance of the Maxcut100 class to be used in tests.
+    """
+    return Maxcut100()
+
+def test_maxcut100():
+    # Maxcut100 sınıfı örneği oluştur
+    problem = Maxcut100()
+
+    # Test durumu: Bütün değerler 0
+    chromosome = [0] * 100
+    expected_result = 0.0 
+    assert problem.f(chromosome) == expected_result, f"Beklenen: {expected_result}, Bulunan: {problem.f(chromosome)}"
+
+    # Test durumu: Bütün değerler 1
+    chromosome = [1] * 100
+    expected_result = 0.0
+    assert problem.f(chromosome) == expected_result, f"Beklenen: {expected_result}, Bulunan: {problem.f(chromosome)}"
+
+    # Test durumu: Karışık değerler
+    chromosome = [0, 1] * 50
+    expected_result = 1124.0 
+    assert problem.f(chromosome) == expected_result, f"Beklenen: {expected_result}, Bulunan: {problem.f(chromosome)}"
+
+if __name__ == "__main__":
+    pytest.main()
+
