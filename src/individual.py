92792a4c370281718a2183ed26286e4a8002cc4a
mhsatman@gmail.com <mhsatman@gmail.com>
2024-08-09 21:25:09 +0300

    update code comments

--- src/individual.py
+++ src/individual.py
@@ -5,8 +5,11 @@ import random as rd
 from problems.abstract_problem import AbstractProblem
 
 
-
 class GeneType(Enum):
+    """
+    GeneType is an enumeration class that represents the type of genome representation for an individual in an evolutionary algorithm.
+    The three types of genome representation are BINARY, PERMUTATION, and REAL.
+    """
     BINARY = 1
     PERMUTATION = 2
     REAL = 3
